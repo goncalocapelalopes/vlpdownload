27eb7751518ed5658ac4b9b8e7221ccb4f7d4d0c
goncalocapelalopes <goncalo.capela.lopes@gmail.com>
2021-10-17 15:32:19 +0100

    Implement song download functionality for an arbitrary artist/album.

--- download.py
+++ download.py
@@ -2,6 +2,7 @@ import os
 import urllib.request
 
 from bs4 import BeautifulSoup
+import requests
 
 ROOT_URL = "https://vaporwave.ivan.moe/list/"
 DESTINATION = "E:\Vaporwave Library"
@@ -12,7 +13,7 @@ HEADERS = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:10.0) \
 
 def get_artists(root_url=ROOT_URL, http_headers=HEADERS):
     """Obtain list of artist names in root directory."""
-    req = urllib.request.Request(url=ROOT_URL, headers=http_headers)
+    req = urllib.request.Request(url=root_url, headers=http_headers)
     html = urllib.request.urlopen(req).read()
     soup = BeautifulSoup(html, "html.parser")
 
@@ -26,6 +27,57 @@ def get_artists(root_url=ROOT_URL, http_headers=HEADERS):
     
     return artist_list
 
+def get_album(artist_list, artist_name, album_name,
+              root_url=ROOT_URL, http_headers=HEADERS,):
+    if artist_name not in artist_list:
+        raise Exception("Invalid artist name.")
+    artist_url = root_url + artist_name + "/"
+    artist_url = artist_url.replace(" ", "%20")
+    req = urllib.request.Request(url=artist_url, headers=http_headers)
+    html = urllib.request.urlopen(req).read()
+    soup = BeautifulSoup(html, "html.parser")
+    
+    # album list is inside a <pre> tag
+    # each album name is in a hyperlink <a> to the respective album folder
+    album_hrefs = soup.find("pre").find_all("a")
+
+    # first element of the list is the parent directory "\.." link
+    # child text alwas ends in "/" so .text[:-1] removes that char
+    album_list = [child.text[:-1] for child in album_hrefs][1:]
+    
+    if album_name not in album_list:
+        raise Exception("Invalid album name.")
+    
+    album_url = artist_url + album_name + "/"
+    album_url = album_url.replace(" ", "%20")
+    req = urllib.request.Request(url=album_url, headers=http_headers)
+    html = urllib.request.urlopen(req).read()
+    soup = BeautifulSoup(html, "html.parser")
+    
+    song_hrefs = soup.find("pre").find_all("a")
+
+    # first element of the list is the parent directory "\.." link
+    # child text alwas ends in "/" so .text[:-1] removes that char
+    song_list = [child.text for child in song_hrefs][1:]
+    artist_folder = os.path.join(DESTINATION, artist_name)
+    if not os.path.exists(artist_folder):
+        os.mkdir(artist_folder)
+    album_folder = os.path.join(artist_folder, album_name)
+    if not os.path.exists(album_folder):
+        os.mkdir(album_folder)
+    print("DOWNLOADING "+ artist_name)
+    print("*"*20)
+    for song_name in song_list:
+        filename = os.path.join(album_folder, song_name)
+        song_url = album_url + song_name
+        song_url = song_url.replace(" ", "%20")
+        print(song_name)
+        
+        r = requests.get(album_url)
+        with open(filename, "wb+") as code:
+            code.write(r.content)
+    
 
 if __name__ == "__main__":
     artists = get_artists()
+    get_album(artists,"Vektroid", "Vektroid - Neo Cali (2011)")
