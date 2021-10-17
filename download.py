114fcb809a52a75bd24f666f80965688068dedbf
goncalocapelalopes <goncalo.capela.lopes@gmail.com>
2021-10-17 11:11:15 +0100

    first

---
+++ download.py
@@ -0,0 +1,13 @@
+import os
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+ROOT_URL = "https://vaporwave.ivan.moe/list/"
+DESTINATION = "E:\Vaporwave Library"
+
+header= {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0"}
+
+req = urllib.request.Request(url=ROOT_URL, headers=header)
+page = urllib.request.urlopen(req).read()
+print(page)
